30885c39eb6911c68cd4dd8d99902907c3d61b5a
Grzegorz <grzegorzc66@gmail.com>
2023-02-09 20:33:25 +0100

    init

---
+++ lib/panoramix.ex
@@ -0,0 +1,18 @@
+defmodule Panoramix do
+  @moduledoc """
+  Documentation for `Panoramix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Panoramix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/panoramix_test.exs
@@ -0,0 +1,8 @@
+defmodule PanoramixTest do
+  use ExUnit.Case
+  doctest Panoramix
+
+  test "greets the world" do
+    assert Panoramix.hello() == :world
+  end
+end
